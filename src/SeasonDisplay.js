d49bebc14ecb6ce9bd547f39f89d8f52f2bb785e
sezaite <martyna.sezaite@gmail.com>
2021-03-24 07:57:28 +0000

    isluptas error

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function SeasonDisplay(props) {
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
 
-    return (
-        <div>
-            Season Display
-        </div>
-    )
-}
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
 
-export default SeasonDisplay
+    console.log(season);
+
+    return <div>Season Display</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -5,30 +5,24 @@ import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
     state = { lat: null, errorMessage: '' };
 
-    conponentDidMount() {
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }),
-            (err) => this.setState({ errorMessage: err.message })
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
-    componentDidUpdate() {
-
-    }
-    componentWillMount() {
-
-    }
-
-
+    // React says we have to define render!!
     render() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <SeasonDisplay err={this.state.errorMessage} />
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
+            return <SeasonDisplay lat={this.state.lat} />;
         }
-        return <div>Loading...</div>
+
+        return <div>Loading!</div>;
     }
 }
 
